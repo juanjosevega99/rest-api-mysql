d95361f80a625dbaaa567daa5cc8b6fe5a17c454
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2022-11-03 21:37:59 -0500

    feat: add profile to typeorm

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Profile } from './profile.entity';
 import { User } from './user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Profile])],
   controllers: [UsersController],
   providers: [UsersService],
 })
