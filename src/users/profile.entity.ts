26fd215952827d7e0e9274da66695cc355fbce2b
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2022-11-03 20:59:37 -0500

    chore: add profile entity

---
+++ src/users/profile.entity.ts
@@ -0,0 +1,16 @@
+import { PrimaryGeneratedColumn, Column, Entity } from 'typeorm';
+
+@Entity()
+export class Profile {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column({ nullable: true })
+  age: number;
+}
