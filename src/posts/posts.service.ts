afb8c4910fd4071e71a9c311d584b10506b56829
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2022-11-13 08:00:57 -0500

    chore: base postService

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UsersService } from 'src/users/users.service';
+import { Repository } from 'typeorm';
+import { Post } from './post.entity';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(
+    @InjectRepository(Post) private postRepository: Repository<Post>,
+    private userService: UsersService,
+  ) {}
+
+  createPost() {}
+  getPost() {}
+}
